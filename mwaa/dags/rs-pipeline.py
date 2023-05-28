f848d3f292edbf3d4c395b53bcccc36462e5b684
Paul Villena <pvillena@amazon.com>
2023-05-28 22:17:23 +1000

    Update workshop

--- mwaa/dags/rs-pipeline.py
+++ mwaa/dags/rs-pipeline.py
@@ -1,8 +1,21 @@
+# Import required objects from airflow library.
 # Add code
 
 
-
+# Setup the constants
 # Add code
 
 
+# Add a DAG Schedule
 # Add code
+
+
+# Run Crawler to detect if there are changes to the schema
+# Add code
+
+
+# Deploy Glue Job and add the `Glue` run operators
+# Add code
+
+
+# Add Redshift Data Operator to create base tables in Redshift and or truncate
